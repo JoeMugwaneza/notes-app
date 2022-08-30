71b2a092f8e9071f573ee5729777917a5f776749
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-30 05:46:54 +0200

    typed editor code and reviewed to understand them

---
+++ src/components/Editor.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Showdown, { Converter } from 'showdown';
+
+export default function Editor({currentNote, updateNote}){
+    const [selectedTab, setSelectedTap] = React.useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true, 
+        simplifiedAutoLink: true, 
+        strikethrough: true,
+        tasklist: true
+    })
+
+
+    return (
+        <section className='pane editor'>
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTap}
+                generateMarkDownPreview={(markdown) => 
+                    Promise.resolve(Converter.makeHtml(markdown))}
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+
+
+        </section>
+    )
+}
